4858526b86816b970bfcbbbe556168fb81c60728
Sergey Pedan <sergey.pedan@gmail.com>
2018-04-04 04:33:30 +0300

    Syntax

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ Opensit::Application.configure do
   config.active_record.mass_assignment_sanitizer = :strict
 
   # Required by devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Do not compress assets
   config.assets.compress = false
