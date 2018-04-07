048f239d301bad1a4bd416135b9876491f127701
Sergey Pedan <sergey.pedan@gmail.com>
2018-04-08 02:47:03 +0300

    Inherits UserMailer from ApplicationMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,37 +1,35 @@
-class UserMailer < ActionMailer::Base
-
-  default from: "OpenSit <#{Rails.application.secrets.public_email}>"
+class UserMailer < ApplicationMailer
 
   def new_design(user)
     @email = user.email
-    @name = user.display_name
+    @name  = user.display_name
 
     mail(to: @email, subject: "Psst.. OpenSit has changed!")
 	end
 
 	def welcome_email(user)
 		@email = user.email
-		mail(to: @email, subject: 'Welcome to the community!') if user.email
+		mail(to: @email, subject: 'Welcome to the community!') if @email
 	end
 
 	def blog_is_live(user)
 		@email = user.email
-		mail(to: @email, subject: 'The OpenSit Blog is Live!') if user.email
+		mail(to: @email, subject: 'The OpenSit Blog is Live!') if @email
 	end
 
   def goals_are_live(user)
     @email = user.email
-    mail(to: @email, subject: 'Set goals for your meditation') if user.email
+    mail(to: @email, subject: 'Set goals for your meditation') if @email
   end
 
   # 11th Jan 2015
   def calendar_intro_email(user)
     @email = user.email
-    mail(to: @email, subject: 'Meditate with people from around the globe!') if user.email
+    mail(to: @email, subject: 'Meditate with people from around the globe!') if @email
   end
 
   def opensit_survey(user)
     @email = user.email
-    mail(to: @email, subject: 'OpenSit is changing... what do you think?') if user.email
+    mail(to: @email, subject: 'OpenSit is changing... what do you think?') if @email
   end
 end
