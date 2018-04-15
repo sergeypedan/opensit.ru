3971fe716078406a1bafe273856a635faaf00c63
Sergey Pedan <sergey.pedan@gmail.com>
2018-04-15 06:18:14 +0300

    Adjusts search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
 
 	def main
-		@users      = User.fuzzy_search(params[:q]).paginate(page: params[:page])
+		# @users      = User.fuzzy_search(params[:q]).paginate(page: params[:page])
+    @users      = []
 		@sits       = Sit.basic_search(params[:q]).communal.paginate(page: params[:page])
 		@tagged     = Sit.tagged_with(params[:q]).communal.paginate(page: params[:page]) if Tag.find_by(name: params[:q])
 		@page_class = "search-results"
