51ebddd67cfce57088d32303b738765dadd772fa
Sergey Pedan <sergey.pedan@gmail.com>
2018-04-15 03:09:06 +0300

    Improves routes alignment

--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,31 @@ Opensit::Application.routes.draw do
   root to: 'pages#front'
 
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
-  get 'me' => 'users#me'
+
+  get '/me', to: 'users#me'
 
   constraints(:username => /[^\/]+/) do
-    get '/u/:username',           to: 'users#show',      as: :user
-    get '/u/:username/following', to: 'users#following', as: :following_user
-    get '/u/:username/followers', to: 'users#followers', as: :followers_user
-    get '/u/:username/export',    to: 'users#export'
-    get '/u/:username/feed',      to: 'users#feed',      as: :feed, defaults: { format: 'atom' }
-    delete '/u/:username',        to: 'users#destroy',   as: :delete_user
+    get    '/u/:username',           to: 'users#show',      as: :user
+    get    '/u/:username/following', to: 'users#following', as: :following_user
+    get    '/u/:username/followers', to: 'users#followers', as: :followers_user
+    get    '/u/:username/export',    to: 'users#export'
+    get    '/u/:username/feed',      to: 'users#feed',      as: :feed, defaults: { format: 'atom' }
+    delete '/u/:username',           to: 'users#destroy',   as: :delete_user
   end
 
-  get 'about',         to: 'pages#about'
-  get 'about-journal', to: 'pages#about_journal', as: :about_journal
-  get 'calendar',      to: 'pages#calendar', as: :calendar
-  get 'contribute',    to: 'pages#contribute'
-  get 'contact',       to: 'pages#contact'
+  get '/about',         to: 'pages#about'
+  get '/about-journal', to: 'pages#about_journal', as: :about_journal
+  get '/calendar',      to: 'pages#calendar', as: :calendar
+  get '/contribute',    to: 'pages#contribute'
+  get '/contact',       to: 'pages#contact'
 
-  get 'explore',                   to: 'pages#explore'
-  get 'explore/tags',              to: 'pages#tag_cloud',    as: :explore_tags
-  get 'explore/comments',          to: 'pages#new_comments', as: :explore_comments
-  get 'explore/users/online',      to: 'pages#online_users', as: :explore_online_users
-  get 'explore/users/new',         to: 'pages#new_users',    as: :explore_new_users
-  get 'explore/users/active',      to: 'pages#active_users', as: :explore_active_users
-  get 'explore/users/new/sitters', to: 'pages#new_sitters',  as: :explore_new_sitters
+  get '/explore',                   to: 'pages#explore'
+  get '/explore/tags',              to: 'pages#tag_cloud',    as: :explore_tags
+  get '/explore/comments',          to: 'pages#new_comments', as: :explore_comments
+  get '/explore/users/online',      to: 'pages#online_users', as: :explore_online_users
+  get '/explore/users/new',         to: 'pages#new_users',    as: :explore_new_users
+  get '/explore/users/active',      to: 'pages#active_users', as: :explore_active_users
+  get '/explore/users/new/sitters', to: 'pages#new_sitters',  as: :explore_new_sitters
 
   get '/global-feed',   to: 'users#feed', defaults: { format: 'atom', scope: 'global' }
   get '/favs',          to: 'favourites#index', as: :favs
@@ -40,12 +41,12 @@ Opensit::Application.routes.draw do
     resources :comments
   end
 
-  resources :favourites, only: [:create, :destroy]
+  resources :favourites,    only: [:create, :destroy]
   resources :goals
-  resources :likes, only: [:create, :destroy]
-  resources :messages, except: [:edit, :update]
+  resources :likes,         only: [:create, :destroy]
+  resources :messages,      except: [:edit, :update]
   resources :relationships, only: [:create, :destroy]
-  resources :reports, only: [:create]
+  resources :reports,       only: [:create]
 
   # Crawl live site, but not staging
   get 'robots.txt', to: 'pages#robots'
