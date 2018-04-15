53cfd8bb00c61b097fc81ba85ee696cc7d7fb24d
Sergey Pedan <sergey.pedan@gmail.com>
2018-04-15 03:54:28 +0300

    Syntax

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
 	def main
 		@users      = User.fuzzy_search(params[:q]).paginate(page: params[:page])
 		@sits       = Sit.basic_search(params[:q]).communal.paginate(page: params[:page])
-		@tagged     = Sit.tagged_with(params[:q]).communal.paginate(page: params[:page]) if Tag.find_by_name(params[:q]) != nil
+		@tagged     = Sit.tagged_with(params[:q]).communal.paginate(page: params[:page]) if Tag.find_by(name: params[:q])
 		@page_class = "search-results"
 		@title      = "Search: #{params[:q]}"
 		render %w[tagged users].include?( params[:type] ) ? params[:type] : "sits"
