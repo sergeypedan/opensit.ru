c8531b05ccc721fdbdfbd465e3206f2376e41e75
Sergey Pedan <sergey.pedan@gmail.com>
2018-04-15 03:52:56 +0300

    Refactors controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,12 @@
 class SearchController < ApplicationController
 
-  def main
-  	@users      = User.fuzzy_search(params[:q]).paginate(page: params[:page])
-  	@sits       = Sit.basic_search(params[:q]).communal.paginate(page: params[:page])
-    @tagged     = Sit.tagged_with(params[:q]).communal.paginate(page: params[:page]) if Tag.find_by_name(params[:q]) != nil
-    @page_class = "search-results"
-    @title      = "Search: #{params[:q]}"
-
-  	type = params[:type]
-
-	  if !type
-  	 render 'sits'
-    elsif type == 'users'
-  		render 'users'
-  	elsif type == 'tagged'
-      render 'tagged'
-    else
-  		render 'sits'
-  	end
-  end
+	def main
+		@users      = User.fuzzy_search(params[:q]).paginate(page: params[:page])
+		@sits       = Sit.basic_search(params[:q]).communal.paginate(page: params[:page])
+		@tagged     = Sit.tagged_with(params[:q]).communal.paginate(page: params[:page]) if Tag.find_by_name(params[:q]) != nil
+		@page_class = "search-results"
+		@title      = "Search: #{params[:q]}"
+		render %w[tagged users].include?( params[:type] ) ? params[:type] : "sits"
+	end
 
 end
