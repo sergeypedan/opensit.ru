3d926aec58cdfd76d322388ebc21f9986bd34897
Sergey Pedan <sergey.pedan@gmail.com>
2018-05-13 23:07:33 +0300

    Refactors likers button

--- app/helpers/sits_helper.rb
+++ app/helpers/sits_helper.rb
@@ -21,4 +21,16 @@ module SitsHelper
 		'<div class="private-event pull-right" title="This is a private entry. Only you can see it."><i class="fa fa-lock"></i></div>'.html_safe
 	end
 
+	def liked_names(sit)
+		return "" if (current_user != sit.user) && sit.likers.empty?
+		return (sit.likers.map { |user| user.display_name } * ', ')
+	end
+
+	def like_button_text(sit)
+		return 'Like it?' if (current_user != sit.user) && sit.likers.empty?
+		people  = pluralize(sit.likes.count, 'person')
+		do_like = sit.likes.count == 1 ? "likes" : "like"
+		"#{people} #{do_like} this"
+	end
+
 end
