a4c309889ce21f9744e9e73342af27c2a1d347ce
Sergey Pedan <sergey.pedan@gmail.com>
2018-05-14 06:33:33 +0300

    Protects tests from wrong env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
+
+abort("The Rails environment is running in #{Rails.env} mode!") unless Rails.env.test?
+
 require File.expand_path("../../config/environment", __FILE__)
+
 require 'rspec/rails'
 require 'capybara/poltergeist'
+
 Capybara.default_driver = :poltergeist
 # Capybara.default_wait_time = 10
 
