910d23199149375dceb5e16a171e8a3c4ba8807f
Sergey <prosto_tak_for_me@rambler.ru>
2020-01-01 19:01:12 +0300

    Translate email subjects.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,32 +4,32 @@ class UserMailer < ApplicationMailer
     @email = user.email
     @name  = user.display_name
 
-    mail(to: @email, subject: "Psst.. OpenSit has changed!")
+    mail(to: @email, subject: I18n.t('email.subject.new_design'))
 	end
 
 	def welcome_email(user)
 		@email = user.email
-		mail(to: @email, subject: 'Welcome to the community!') if @email
+		mail(to: @email, subject: I18n.t('email.subject.welcome_email')) if @email
 	end
 
 	def blog_is_live(user)
 		@email = user.email
-		mail(to: @email, subject: 'The OpenSit Blog is Live!') if @email
+		mail(to: @email, subject: I18n.t('email.subject.blog_is_live')) if @email
 	end
 
   def goals_are_live(user)
     @email = user.email
-    mail(to: @email, subject: 'Set goals for your meditation') if @email
+    mail(to: @email, subject: I18n.t('email.subject.goals_are_live')) if @email
   end
 
   # 11th Jan 2015
   def calendar_intro_email(user)
     @email = user.email
-    mail(to: @email, subject: 'Meditate with people from around the globe!') if @email
+    mail(to: @email, subject: I18n.t('email.subject.calendar_intro_email')) if @email
   end
 
   def opensit_survey(user)
     @email = user.email
-    mail(to: @email, subject: 'OpenSit is changing... what do you think?') if @email
+    mail(to: @email, subject: I18n.t('email.subject.opensit_survey')) if @email
   end
 end
