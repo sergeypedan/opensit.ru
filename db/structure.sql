83a6e2fcfdd94f46236d583343dd7f73cd3694e8
Sergey Pedan <sergey.pedan@gmail.com>
2020-01-06 10:55:49 +0700

    Some migration artifacts (PG12 used)

--- db/structure.sql
+++ db/structure.sql
@@ -5,26 +5,13 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
 
 --
 -- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
@@ -510,7 +497,7 @@ CREATE TABLE public.users (
     website character varying(100),
     avatar_file_name character varying,
     avatar_content_type character varying,
-    avatar_file_size integer,
+    avatar_file_size bigint,
     avatar_updated_at timestamp without time zone,
     private_stream boolean DEFAULT false,
     reset_password_sent_at timestamp without time zone,
