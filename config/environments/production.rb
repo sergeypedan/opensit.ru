800b8e4215ed8f33e911acfa77230c2009df241f
Sergey Pedan <sergey.pedan@gmail.com>
2020-01-06 16:00:09 +0700

    Enables static files serving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ Opensit::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = false
+  config.serve_static_files = true # Heroku
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -77,4 +78,14 @@ Opensit::Application.configure do
   config.active_support.deprecation = :notify
 
   config.eager_load = true
+
+
+  # Log
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
 end
