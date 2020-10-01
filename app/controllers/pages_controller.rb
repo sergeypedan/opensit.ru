a29f045fd14e0abb346dd80ddca9925f229b49c3
Sergey <prosto_tak_for_me@rambler.ru>
2020-10-01 17:58:48 +0300

    Reduce number of sits on the landing page to 10

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def front
     return redirect_to controller: :users, action: :me if user_signed_in?
-    @sits       = Sit.communal.with_body.newest_first.limit(30)
+    @sits       = Sit.communal.with_body.newest_first.limit(10)
     @page_class = 'front-page'
     render "front", layout: "minimal"
   end
