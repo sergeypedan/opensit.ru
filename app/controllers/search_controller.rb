6053106af426522eb2e1ba634403b2d3f84726e2
Sergey <prosto_tak_for_me@rambler.ru>
2021-01-21 00:26:58 +0300

    OP-47: Fix empty search collections checking.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,5 @@
 class SearchController < ApplicationController
+	helper_method :rendered_template
 
 	def main
 		# @users      = User.fuzzy_search(params[:q]).paginate(page: params[:page])
@@ -7,7 +8,15 @@ class SearchController < ApplicationController
 		@tagged     = Sit.tagged_with(params[:q]).communal.paginate(page: params[:page]) if Tag.find_by(name: params[:q])
 		@page_class = "search-results"
 		@title      = t('search.title', query: params[:q])
-		render %w[tagged users].include?( params[:type] ) ? params[:type] : "sits"
+
+		render rendered_template
 	end
 
+	private
+
+	def rendered_template
+		return 'sits' if (params[:type] == 'sits') || (params[:type] == 'tagged' && @tagged.blank?)
+
+		params[:type]
+	end
 end
